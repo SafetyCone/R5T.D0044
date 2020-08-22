0734eee065543ac932332cee3a55db0f6451c793
David Coats <david@getsafetycone.com>
2020-08-22 11:04:33 -0700

    Add R5T.D0044.Base project and IStringSerializer<T> service definition

---
+++ source/R5T.D0044.Base/Code/Services/Definitions/IStringSerializer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+
+namespace R5T.D0044
+{
+    public interface IStringSerializer<T>
+    {
+        Task<T> Deserialize(string @string);
+
+        Task<string> Serialize(T value);
+    }
+}

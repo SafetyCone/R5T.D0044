c818852bd60c47fcecfa1f3f796056ad1a92dc99
David Coats <david@getsafetycone.com>
2020-08-29 17:10:19 -0700

    Add R5T.D0044.Default (TokenizedStringSerializerBase<T>) library project

---
+++ source/R5T.D0044.Default/Code/Services/Implementations/Base Classes/TokenizedStringSerializerBase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using R5T.Magyar;
+
+
+namespace R5T.D0044.Default
+{
+    public abstract class TokenizedStringSerializerBase<T> : IStringSerializer<T>
+    {
+        public const char DefaultTokenSeparatorChar = '|';
+
+
+        public virtual char TokenSeparatorChar => TokenizedStringSerializerBase<T>.DefaultTokenSeparatorChar;
+        public virtual char[] Separators => ArrayHelper.From(this.TokenSeparatorChar);
+
+
+        public Task<T> Deserialize(string @string)
+        {
+            var tokens = @string.Split(this.Separators);
+
+            return this.DeserializeTokens(tokens);
+        }
+
+        protected abstract Task<T> DeserializeTokens(string[] tokens);
+
+        public async Task<string> Serialize(T value)
+        {
+            var tokens = await this.GetTokens(value);
+
+            var result = String.Join(this.TokenSeparatorChar.ToString(), tokens);
+            return result;
+        }
+
+        public abstract Task<string[]> GetTokens(T value);
+    }
+}
